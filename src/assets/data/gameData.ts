4d4bc30a8dbb10d093be7f47692ed6cebf526243
lhinxue <lhinxue@gmail.com>
2025-06-14 11:38:58 +1200

    Resolve Image Issue

--- src/assets/data/gameData.ts
+++ src/assets/data/gameData.ts
@@ -187,338 +187,338 @@ export const gameAvatar: { [name: string]: UserAvatar } = {
 export const gameCharacter: { [name: string]: Character } = {
   大黑塔: {
     name: '大黑塔',
-    avatar: 'src/assets/images/character/the-herta-character_icon.webp',
+    avatar: '/images/character/the-herta-character_icon.webp',
     info: '是本人'
   },
   阿格莱雅: {
     name: '阿格莱雅',
-    avatar: 'src/assets/images/character/aglaea-character_icon.webp',
+    avatar: '/images/character/aglaea-character_icon.webp',
     info: '浴场见'
   },
   缇宝: {
     name: '缇宝',
-    avatar: 'src/assets/images/character/tribbie-character_icon.webp',
+    avatar: '/images/character/tribbie-character_icon.webp',
     info: '缇宝老师随时在哦~'
   },
   万敌: {
     name: '万敌',
-    avatar: 'src/assets/images/character/mydei-character_icon.webp',
+    avatar: '/images/character/mydei-character_icon.webp',
     info: '三分练，七分吃'
   },
   遐蝶: {
     name: '遐蝶',
-    avatar: 'src/assets/images/character/castorice-character_icon.webp',
+    avatar: '/images/character/castorice-character_icon.webp',
     info: '在写了'
   },
   那刻夏: {
     name: '那刻夏',
-    avatar: 'src/assets/images/character/anaxa-character_icon.webp',
+    avatar: '/images/character/anaxa-character_icon.webp',
     info: '那我问你'
   },
   风堇: {
     name: '风堇',
-    avatar: 'src/assets/images/character/hyacine-character_icon.webp',
+    avatar: '/images/character/hyacine-character_icon.webp',
     info: '昏光庭院，一直在你身边~'
   },
   赛飞儿: {
     name: '赛飞儿',
-    avatar: 'src/assets/images/character/cipher-character_icon.webp',
+    avatar: '/images/character/cipher-character_icon.webp',
     info: '来财！'
   },
   忘归人: {
     name: '停云',
-    avatar: 'src/assets/images/character/fugue-character_icon.webp',
+    avatar: '/images/character/fugue-character_icon.webp',
     info: '有话好好说，不要吵架~'
   },
   乱破: {
     name: '乱破',
-    avatar: 'src/assets/images/character/rappa-character_icon.webp',
+    avatar: '/images/character/rappa-character_icon.webp',
     info: '一心不乱，破邪显正'
   },
   灵砂: {
     name: '灵砂',
-    avatar: 'src/assets/images/character/lingsha-character_icon.webp',
+    avatar: '/images/character/lingsha-character_icon.webp',
     info: '莫生气，莫生气，哪有功夫生闲气'
   },
   飞霄: {
     name: '飞霄',
-    avatar: 'src/assets/images/character/feixiao-character_icon.webp',
+    avatar: '/images/character/feixiao-character_icon.webp',
     info: '手痒'
   },
   云璃: {
     name: '云璃',
-    avatar: 'src/assets/images/character/yunli-character_icon.webp',
+    avatar: '/images/character/yunli-character_icon.webp',
     info: '(´ ･ ω ･ `)打吗？'
   },
   翡翠: {
     name: '翡翠',
-    avatar: 'src/assets/images/character/jade-character_icon.webp',
+    avatar: '/images/character/jade-character_icon.webp',
     info: '欢迎随时光临「慈玉典押」'
   },
   流萤: {
     name: '流萤',
-    avatar: 'src/assets/images/character/firefly-character_icon.webp',
+    avatar: '/images/character/firefly-character_icon.webp',
     info: '会找到的，属于我的梦...'
   },
   知更鸟: {
     name: '知更鸟',
-    avatar: 'src/assets/images/character/robin-character_icon.webp',
+    avatar: '/images/character/robin-character_icon.webp',
     info: '让我们把翅膀借给彼此'
   },
   黄泉: {
     name: '黄泉',
-    avatar: 'src/assets/images/character/acheron-character_icon.webp',
+    avatar: '/images/character/acheron-character_icon.webp',
     info: '该启程了。'
   },
   花火: {
     name: '花火',
-    avatar: 'src/assets/images/character/sparkle-character_icon.webp'
+    avatar: '/images/character/sparkle-character_icon.webp'
   },
   黑天鹅: {
     name: '黑天鹅',
-    avatar: 'src/assets/images/character/black-swan-character_icon.webp',
+    avatar: '/images/character/black-swan-character_icon.webp',
     info: '记忆是柔软的琥珀'
   },
   '阮·梅': {
     name: '阮·梅',
-    avatar: 'src/assets/images/character/ruan-mei-character_icon.webp',
+    avatar: '/images/character/ruan-mei-character_icon.webp',
     info: '新口味的糕点...哪里买？'
   },
   藿藿: {
     name: '藿藿',
-    avatar: 'src/assets/images/character/huohuo-character_icon.webp',
+    avatar: '/images/character/huohuo-character_icon.webp',
     info: '尾巴大爷的小跟班'
   },
   '托帕&账账': {
     name: '托帕',
-    avatar: 'src/assets/images/character/topaz-numby-character_icon.webp',
+    avatar: '/images/character/topaz-numby-character_icon.webp',
     info: '出差中~小事留言，大事语音'
   },
   镜流: {
     name: '镜流',
-    avatar: 'src/assets/images/character/jingliu-character_icon.webp',
+    avatar: '/images/character/jingliu-character_icon.webp',
     info: ''
   },
   符玄: {
     name: '符玄',
-    avatar: 'src/assets/images/character/fu-xuan-character_icon.webp',
+    avatar: '/images/character/fu-xuan-character_icon.webp',
     info: '不卜人事福祸！'
   },
   卡芙卡: {
     name: '卡芙卡',
-    avatar: 'src/assets/images/character/kafka-character_icon.webp',
+    avatar: '/images/character/kafka-character_icon.webp',
     info: ''
   },
   银狼: {
     name: '银狼',
-    avatar: 'src/assets/images/character/silver-wolf-character_icon.webp',
+    avatar: '/images/character/silver-wolf-character_icon.webp',
     info: '不会做游戏就不要做'
   },
   白露: {
     name: '白露',
-    avatar: 'src/assets/images/character/bailu-character_icon.webp',
+    avatar: '/images/character/bailu-character_icon.webp',
     info: '多喝热水少生气'
   },
   希儿: {
     name: '希儿',
-    avatar: 'src/assets/images/character/seele-character_icon.webp',
+    avatar: '/images/character/seele-character_icon.webp',
     info: '有话直说！！'
   },
   克拉拉: {
     name: '克拉拉',
-    avatar: 'src/assets/images/character/clara-character_icon.webp',
+    avatar: '/images/character/clara-character_icon.webp',
     info: '想和大家去野餐(>▽<)'
   },
   布洛妮娅: {
     name: '布洛妮娅',
-    avatar: 'src/assets/images/character/bronya-character_icon.webp',
+    avatar: '/images/character/bronya-character_icon.webp',
     info: '会议中'
   },
   姬子: {
     name: '姬子',
-    avatar: 'src/assets/images/character/himeko-character_icon.webp',
+    avatar: '/images/character/himeko-character_icon.webp',
     info: '可以不喝水，但不能没咖啡'
   },
   星期日: {
     name: '星期日',
-    avatar: 'src/assets/images/character/sunday-character_icon.webp',
+    avatar: '/images/character/sunday-character_icon.webp',
     info: '♪正在收听《若我不曾见过太阳》'
   },
   椒丘: {
     name: '椒丘',
-    avatar: 'src/assets/images/character/jiaoqiu-character_icon.webp',
+    avatar: '/images/character/jiaoqiu-character_icon.webp',
     info: '没有一顿火锅解决不了的事'
   },
   波提欧: {
     name: '波提欧',
-    avatar: 'src/assets/images/character/boothill-character_icon.webp',
+    avatar: '/images/character/boothill-character_icon.webp',
     info: '庇尔波因特标准差事3=1'
   },
   砂金: {
     name: '砂金',
-    avatar: 'src/assets/images/character/aventurine-character_icon.webp',
+    avatar: '/images/character/aventurine-character_icon.webp',
     info: '长期接代抽业务'
   },
   真理医生: {
     name: '真理医生',
-    avatar: 'src/assets/images/character/dr-ratio-character_icon.webp',
+    avatar: '/images/character/dr-ratio-character_icon.webp',
     info: '「不急」'
   },
   银枝: {
     name: '银枝',
-    avatar: 'src/assets/images/character/argenti-character_icon.webp',
+    avatar: '/images/character/argenti-character_icon.webp',
     info: '向一朵玫瑰起誓'
   },
   '丹恒•饮月': {
     name: '丹恒',
-    avatar: 'src/assets/images/character/dan-heng-imbibitor-lunae-character_icon.webp',
+    avatar: '/images/character/dan-heng-imbibitor-lunae-character_icon.webp',
     info: ''
   },
   刃: {
     name: '刃',
-    avatar: 'src/assets/images/character/blade-character_icon.webp',
+    avatar: '/images/character/blade-character_icon.webp',
     info: ''
   },
   罗刹: {
     name: '罗刹',
-    avatar: 'src/assets/images/character/luocha-character_icon.webp',
+    avatar: '/images/character/luocha-character_icon.webp',
     info: '一介行商'
   },
   景元: {
     name: '景元',
-    avatar: 'src/assets/images/character/jing-yuan-character_icon.webp',
+    avatar: '/images/character/jing-yuan-character_icon.webp',
     info: '人不在神策府'
   },
   彦卿: {
     name: '彦卿',
     avatar:
-      'src/assets/images/character/yanqing-character_icon.webp',
+      '/images/character/yanqing-character_icon.webp',
     info: '今天工造司上新了么？没有'
   },
   杰帕德: {
     name: '杰帕德',
-    avatar: 'src/assets/images/character/gepard-character_icon.webp',
+    avatar: '/images/character/gepard-character_icon.webp',
     info: '执勤回复较慢，请见谅'
   },
   瓦尔特: {
     name: '瓦尔特',
-    avatar: 'src/assets/images/character/welt-character_icon.webp',
+    avatar: '/images/character/welt-character_icon.webp',
     info: '列车组的各位，随时保持联系'
   },
   '三月七·巡猎': {
     name: '三月七',
-    avatar: 'src/assets/images/character/march-7th-character-2_icon.webp',
+    avatar: '/images/character/march-7th-character-2_icon.webp',
     info: '今天也是三月七~'
   },
   雪衣: {
     name: '雪衣',
-    avatar: 'src/assets/images/character/xueyi-character_icon.webp',
+    avatar: '/images/character/xueyi-character_icon.webp',
     info: '闭棺中，勿扰。'
   },
   桂乃芬: {
     name: '桂乃芬',
-    avatar: 'src/assets/images/character/guinaifen-character_icon.webp',
+    avatar: '/images/character/guinaifen-character_icon.webp',
     info: '不惹事不怕事、不咬打火机'
   },
   寒鸦: {
     name: '寒鸦',
-    avatar: 'src/assets/images/character/hanya-character_icon.webp',
+    avatar: '/images/character/hanya-character_icon.webp',
     info: '梦占中，勿扰。'
   },
   玲可: {
     name: '玲可',
-    avatar: 'src/assets/images/character/lynx-character_icon.webp',
+    avatar: '/images/character/lynx-character_icon.webp',
     info: '您呼叫的用户不在服务区'
   },
   驭空: {
     name: '驭空',
-    avatar: 'src/assets/images/character/yukong-character_icon.webp',
+    avatar: '/images/character/yukong-character_icon.webp',
     info: '好想再一次回到天空...'
   },
   青雀: {
     name: '青雀',
-    avatar: 'src/assets/images/character/qingque-character_icon.webp',
+    avatar: '/images/character/qingque-character_icon.webp',
     info: '工作可以，别耽误我打牌'
   },
   素裳: {
     name: '素裳',
-    avatar: 'src/assets/images/character/sushang-character_icon.webp',
+    avatar: '/images/character/sushang-character_icon.webp',
     info: '一读书就困是什么病症？'
   },
   停云: {
     name: '停云',
-    avatar: 'src/assets/images/character/tingyun-character_icon.webp',
+    avatar: '/images/character/tingyun-character_icon.webp',
     info: '有话好好说，不要吵架~'
   },
   虎克: {
     name: '虎克',
-    avatar: 'src/assets/images/character/hook-character_icon.webp',
+    avatar: '/images/character/hook-character_icon.webp',
     info: '鼹鼠党！搏击俱乐部集合！'
   },
   娜塔莎: {
     name: '娜塔莎',
-    avatar: 'src/assets/images/character/natasha-character_icon.webp',
+    avatar: '/images/character/natasha-character_icon.webp',
     info: '机械聚落巡诊，切勿跑空'
   },
   希露瓦: {
     name: '希露瓦',
-    avatar: 'src/assets/images/character/serval-character_icon.webp',
+    avatar: '/images/character/serval-character_icon.webp',
     info: '缺觉，缺灵感'
   },
   佩拉: {
     name: '佩拉',
     avatar:
-      'src/assets/images/character/pela-character_icon.webp',
+      '/images/character/pela-character_icon.webp',
     info: '情报提供请备注姓名'
   },
   黑塔: {
     name: '黑塔',
-    avatar: 'src/assets/images/character/herta-character_icon.webp',
+    avatar: '/images/character/herta-character_icon.webp',
     info: '此号停用|商务联系：艾丝妲'
   },
   艾丝妲: {                             
     name: '艾丝妲',
-    avatar: 'src/assets/images/character/asta-character_icon.webp',
+    avatar: '/images/character/asta-character_icon.webp',
     info: '不能再买了...'
   },
   三月七: {
     name: '三月七',
-    avatar: 'src/assets/images/character/march-7th-character_icon.webp',
+    avatar: '/images/character/march-7th-character_icon.webp',
     info: '今天也是三月七~'
   },
   貊泽: {
     name: '貊泽',
-    avatar: 'src/assets/images/character/moze-character_icon.webp',
+    avatar: '/images/character/moze-character_icon.webp',
     info: '没有签名。'
   },
   加拉赫: {
     name: '加拉赫',
-    avatar: 'src/assets/images/character/gallagher-character_icon.webp',
+    avatar: '/images/character/gallagher-character_icon.webp',
     info: '匹诺康尼的狗'
   },
   米沙: {
     name: '米沙',
-    avatar: 'src/assets/images/character/misha-character_icon.webp',
+    avatar: '/images/character/misha-character_icon.webp',
     info: '加油！马上就能去新世界啦'
   },
   卢卡: {
     name: '卢卡',
-    avatar: 'src/assets/images/character/luka-character_icon.webp',
+    avatar: '/images/character/luka-character_icon.webp',
     info: '没回就是在训练！'
   },
   桑博: {
     name: '桑博',
-    avatar: 'src/assets/images/character/sampo-character_icon.webp',
+    avatar: '/images/character/sampo-character_icon.webp',
     info: '古代遗物代理，有过验记录'
   },
   阿兰: {
     name: '阿兰',
-    avatar: 'src/assets/images/character/arlan-character_icon.webp',
+    avatar: '/images/character/arlan-character_icon.webp',
     info: '佩佩紧急联系人'
   },
   丹恒: {
     name: '丹恒',
-    avatar: 'src/assets/images/character/dan-heng-character_icon.webp',
+    avatar: '/images/character/dan-heng-character_icon.webp',
     info: '智库相关找我'
   }
 }
